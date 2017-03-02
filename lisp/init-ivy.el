506249cb5b23c42339b7d80fefb58c80f6510ee4
Chen Bin <chenbin.sh@gmail.com>
2017-03-03 10:12:44 +1100

    ivy setup

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -1,5 +1,6 @@
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
 
